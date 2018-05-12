6d5fa6f4d95d7c2fa8c8aeab551c86a8b961818b
jvillacis <juanfv@udel.edu>
2018-05-12 12:47:09 -0400

    Added the S branch

--- Stat_Genuis.R
+++ Stat_Genuis.R
@@ -10,7 +10,7 @@ DI<-as.character(x[2,3])
 x1y1<-c(x1,y1)
 
 if(CSK=="K"){
-  print("Data given is proportional data of successes and failures, performing Z-test of equal proportions")
+  print("Data given is proportional data of successes and failures, performing two-sided Z-test of equal proportions at a 0.05 significance level")
   s1<-sum(x1==1)
   f1<-sum(x1==0)
   s2<-sum(y1==1)
@@ -24,5 +24,20 @@ if(CSK=="K"){
     else
       cat("Pvalue:", proptest$p.value, "Fail to reject null hypothesis")
   }
-}    
+} else if (CSK == "S"){
+  print("Data given for a test of spread, performing F-test")
+  normx1<-shapiro.test(x1)
+  normy1<-shapiro.test(y1)
+  if(normx1$p.value <= 0.05 || normy1$p.value < 0.05)
+    print("F-test aborted, one or more samples not normal")
+  else{
+    ftest<-var.test(x1,y1,alternative = "two.sided")
+    if(ftest$p.value <= 0.05)
+      cat("Pvalue:", ftest$p.value, "Reject the null hypothesis")
+    else
+      cat("Pvalue:", ftest$p.value, "Fail to reject the null hypothesis")
+  }
+  
+}
+
     
